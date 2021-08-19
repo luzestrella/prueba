3323ca11cc2fe92e53dab5e366db8fe4fdd34951
luzestrella <luzestrella.cortes@alumno.uttehuacan.edu.mx>
2021-08-19 18:08:20 -0500

    cambios

--- clientes.php
+++ clientes.php
@@ -2,7 +2,8 @@
 include './Consultas/ConscultaVen.php';
 ?>
 <html>
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
@@ -11,54 +12,60 @@ include './Consultas/ConscultaVen.php';
     <script src="funciones.js" type="text/javascript"></script>
     <link href="estilo/ESTILO.css" rel="stylesheet" type="text/css" />
 
-    </head>
-    <body>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">EVALUACION</a>
+        <a class="navbar-brand" href="index.php">EVALUACION</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-            <a class="nav-link active" href="tablaVen.php">Detalle de venta<span class="sr-only">(current)</span></a>
+                <a class="nav-link active" href="tablaVen.php">Detalle de venta<span class="sr-only">(current)</span></a>
                 <a class="nav-link" href="productos.php">Productos</a>
                 <a class="nav-link" href="clientes.php">Clientes</a>
             </div>
         </div>
     </nav>
-        <div class="container" data-role="page" id="pageone">
-             <div  data-role="header" id="h1">
-                </div>
-            <button id="btn" type="button"  class="btn btn-link"> <a href="index.php">Regresar</a></button>
-            <div ddata-role="main" class="ui-content">
-                <table  class="table table-bordered"  id="myTable">
-                    <br><br><br>
-                    <thead>
-                        <tr>
-                        <th data-priority="1">IdCliente</th>
-                            <th data-priority="2">Ruc</th>
-                            <th data-priority="3">Razon social</th>
-                        
-                        </tr>
-                    </thead>
-                    <tbody >
-
-                        <?php
-                    
-                        while ($cliente = mysqli_fetch_assoc($result2)) {
-                            ?>
-                            <tr>
-                                
-                            <td> <?php echo $cliente['IdCliente']; ?></td>
-                                <td><?php echo $cliente['Ruc']; ?></td>
-                                <td><?php echo $cliente['Razon social']; ?></td>     
-                            </tr>
-                            <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+    <div class="container" data-role="page" id="pageone">
+        <div data-role="header" id="h1">
         </div>
-    </body>
+        <div ddata-role="main" class="ui-content">
+            <table class="table table-bordered" id="myTable">
+                <br><br><br>
+                <thead>
+                    <tr>
+                        <th data-priority="1">IdCliente</th>
+                        <th data-priority="2">Ruc</th>
+                        <th data-priority="3">Razon social</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+
+                    while ($cliente = mysqli_fetch_assoc($result2)) {
+                    ?>
+                        <tr>
+
+                            <td> <?php echo $cliente['IdCliente']; ?></td>
+                            <td><?php echo $cliente['Ruc']; ?></td>
+                            <td><?php echo $cliente['Razon social']; ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <br><br><br>
+        <div class="col text-center">
+            <button id="btn" type="button" class="btn btn-link"> <a href="index.php">Regresar</a></button>
+        </div>
+
+    </div>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -8,44 +8,58 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   <script src="funciones.js" type="text/javascript"></script>
   <script src="funciones.js" type="text/javascript"></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
   <link href="estilo/ESTILO.css" rel="stylesheet" type="text/css" />
 
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">EVALUACION</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="tablaVen.php">Detalle de venta<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="productos.php">Productos</a>
+        <a class="nav-link" href="clientes.php">Clientes</a>
 
-    <form class="dropdown-menu p-4">
-      <div class="form-group">
-        <label for="exampleDropdownFormEmail2">Email address</label>
-        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
       </div>
-      <div class="form-group">
-        <label for="exampleDropdownFormPassword2">Password</label>
-        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-      </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-          <label class="form-check-label" for="dropdownCheck2">
-            Remember me
-          </label>
+    </div>
+  </nav>
+  <br><br><br><br><br>
+  <div class="container">
+    <div class="dropdown-menu">
+      <form class="px-4 py-3">
+        <div class="form-group">
+          <label for="exampleDropdownFormEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
         </div>
-      </div>
-      <button type="submit" class="btn btn-primary">Sign in</button>
-    </form>
+        <div class="form-group">
+          <label for="exampleDropdownFormPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="dropdownCheck">
+            <label class="form-check-label" for="dropdownCheck">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+      </form>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">New around here? Sign up</a>
+      <a class="dropdown-item" href="#">Forgot password?</a>
+    </div>
 
 
 
+  </div>
 
 
 
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="bootstrap-4.5.2-dist/js/bootstrap.js"></script>
 
 
 
